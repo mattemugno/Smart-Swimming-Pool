7085ccdd996a745008e20b54c4dde81ab25a41c6
Buch97 <pucciofilo97@gmail.com>
2023-05-24 12:46:14 -0400

    Initial commit

--- src/main/java/it/unipi/aide/iot/Main.java
+++ src/main/java/it/unipi/aide/iot/Main.java
@@ -9,10 +9,6 @@ public class Main {
         coapServer.start();
         MQTTSubscriber mqttHandler = new MQTTSubscriber();
 
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }

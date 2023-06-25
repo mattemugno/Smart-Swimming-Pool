47d68b9ef824358d778df93fa4a221854a2f57d8
Buch97 <fpucciofilo97@gmail.com>
2023-06-25 18:03:53 +0200

    aggiunte variabili per disattivare i trigger

--- src/main/java/it/unipi/aide/iot/utility/SimulationParameters.java
+++ src/main/java/it/unipi/aide/iot/utility/SimulationParameters.java
@@ -2,6 +2,9 @@ package it.unipi.aide.iot.utility;
 
 public class SimulationParameters {
     private static boolean manualCommandLight = false;
+    private static boolean manualCommandHeater = false;
+    private static boolean manualCommandWaterPump = false;
+    private static boolean manualCommandChlorineDisp = false;
 
     public SimulationParameters() {
     }
@@ -12,5 +15,29 @@ public class SimulationParameters {
 
     public static void setManualCommandLight(boolean manualCommandLight) {
         SimulationParameters.manualCommandLight = manualCommandLight;
+    }
+
+    public static boolean isManualCommandHeater() {
+        return manualCommandHeater;
+    }
+
+    public static void setManualCommandHeater(boolean manualCommandHeater) {
+        SimulationParameters.manualCommandHeater = manualCommandHeater;
+    }
+
+    public static boolean isManualCommandWaterPump() {
+        return manualCommandWaterPump;
+    }
+
+    public static void setManualCommandWaterPump(boolean manualCommandWaterPump) {
+        SimulationParameters.manualCommandWaterPump = manualCommandWaterPump;
+    }
+
+    public static boolean isManualCommandChlorineDisp() {
+        return manualCommandChlorineDisp;
+    }
+
+    public static void setManualCommandChlorineDisp(boolean manualCommandChlorineDisp) {
+        SimulationParameters.manualCommandChlorineDisp = manualCommandChlorineDisp;
     }
 }
